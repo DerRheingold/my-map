2f2b15b834722b7496978bd4634e3ac740b77546
MichaelKo <m.koelsch@topdesk.com>
2021-07-07 09:57:46 +0200

    new try

--- src/desks.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="desks" tilewidth="32" tileheight="32" tilecount="110" columns="10">
- <image source="../../../maps/Floor0/desks.png" width="320" height="352"/>
-</tileset>
